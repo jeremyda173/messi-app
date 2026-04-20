8f7de5f8ff6d5421ccb020aaf80e5322b16f0333
jeremyda173 <jeremygithud173@gmail.com>
2026-04-19 22:56:37 -0400

    feat: add NextMatch widget to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/footer"
 import { Hero } from "@/components/home/hero"
 import { Biography } from "@/components/home/biography"
 import { QuickStats } from "@/components/home/quick-stats"
+import { NextMatch } from "@/components/home/next-match"
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <HeaderNav />
       <main className="flex-1 px-4 md:px-8 py-8">
         <Hero />
+        <NextMatch />
         <Biography />
         <QuickStats />
       </main>
